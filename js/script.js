af4f74290c8ea6ae48fc0986c16bee8ceb024b87
Jose-souza-dev <129606602+Jose-souza-dev@users.noreply.github.com>
2024-08-03 12:06:25 -0300

    0.0.01
    Mudanças na página do cardápio, mudanças e melhoramento no click do produtos  e ingredientes.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleDiv(divId) {
+    var div = document.getElementById(divId);
+    if (div.style.display === "none" || div.style.display === "") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
