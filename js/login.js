c8ffabf9e728339769324fb3b3db8becf65e9cd4
Jose-souza-dev <129606602+Jose-souza-dev@users.noreply.github.com>
2024-11-21 10:18:52 -0300

    Adição de página plano
    A página plano foi criada para mostrar qual plano o assinante tem e mostrar o tempo que falta para acabar.

---
+++ js/login.js
@@ -0,0 +1,22 @@
+
+        function logar() {
+            // Captura os valores dos campos
+            const usuario = document.getElementById("adm").value;
+            const senha = document.getElementById("pass").value;
+
+            // Validação básica
+            if (usuario === "" || senha === "") {
+                alert("Por favor, preencha todos os campos.");
+                return;
+            }
+
+            // Simula validação (substitua pelo seu processo real de autenticação)
+            if (usuario === "admin" && senha === "1234") {
+                window.location.href ="plano.html";
+                // Redirecionar ou outra ação
+                console.log("Usuário autenticado com sucesso.");
+            } else {
+                alert("Usuário ou senha inválidos.");
+                console.log("Falha na autenticação.");
+            }
+        }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-function toggleDiv(divId) {
-    var div = document.getElementById(divId);
-    if (div.style.display === "none" || div.style.display === "") {
-        div.style.display = "block";
-    } else {
-        div.style.display = "none";
-    }
+// Declarando constantes relativas a dias, horas, minutos e segundos.
+// 1º vamos pegar todos os elementos lá no html
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = " 14 nov 2025";
+
+function countdown(){
+
+    const newYearsDate = new Date(newYears);
+    //Constante para a data atual
+    const currentDate = new Date();
+
+    //Constante para representar os segundos
+    const totalSeconds = (newYearsDate - currentDate)/1000;
+
+    // Constante de dias
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+
+}
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
 }
 
+countdown();
+setInterval(countdown, 1000);
